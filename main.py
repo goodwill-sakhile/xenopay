73d6a32dc69f4edafef7297cd487e094f4958ee1
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2023-12-27 14:42:41 +0200

    xenopay main screen script

---
+++ main.py
@@ -0,0 +1,28 @@
+from kivymd.app import MDApp
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.uix.screenmanager import ScreenManager
+from kivy.lang import Builder
+from login import LoginScreen
+from sign_up_screen import SignUpScreen
+from kivy.core.window import Window
+Window.size = (350, 600)
+root = Builder.load_string("""
+<MainBox>:
+    orientation:"vertical"
+    md_bg_color:[47/float(255), 79/float(255), 79/float(255), 1]
+    MDBoxLayout:
+        size_hint_y:None
+        height:"100dp"
+    MDBoxLayout:
+        ScreenManager:
+            LoginScreen:
+            SignUpScreen:
+""")
+class MainBox(MDBoxLayout):
+    pass
+class TestApp(MDApp):
+    def build(self):
+        root = MainBox()
+        return root
+if __name__ == "__main__":
+    TestApp().run()
