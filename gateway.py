0fbdf5fbe4ce495bd7c82704c55ceb07aaf5818a
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2024-01-10 08:59:44 +0200

    gateway script

--- gateway.py
+++ gateway.py
@@ -5,19 +5,29 @@ from kivymd.uix.screen import MDScreen
 from kivy.lang import Builder
 from login import LoginScreen
 from sign_up_screen import SignUpScreen
-from kivy.core.window import Window
+from recovery_password_screen import RecoverPasswordScreen
+#from kivy.core.window import Window
 #Window.size = (350, 600)
 root = Builder.load_string("""
 <GatewayScreen>:
-    orientation:"vertical"
-    md_bg_color:[47/float(255), 79/float(255), 79/float(255), 1]
     MDBoxLayout:
-        size_hint_y:None
-        height:"100dp"
-    MDBoxLayout:
-        ScreenManager:
-            LoginScreen:
-            SignUpScreen:
+    	orientation:"vertical"
+	    md_bg_color:[47/float(255), 79/float(255), 79/float(255), 1]
+	    MDBoxLayout:
+	        size_hint_y:None
+	        height:"100dp"
+	        md_bg_color:0, 0, 0, 1
+	    MDBoxLayout:
+	        ScreenManager:
+	            #RecoverPasswordScreen:
+	            LoginScreen:
+	            SignUpScreen:
 """)
 class GatewayScreen(MDScreen):
     pass
+class TestApp(MDApp):
+	def build(self):
+		root = GatewayScreen()
+		return root
+if __name__ == "__main__":
+	TestApp().run()
