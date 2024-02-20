fdd3b6b2d5495c03532d73c15f1a4441f7b01e9a
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2024-02-20 09:34:44 +0200

    gteway screen script

---
+++ gateway.py
@@ -0,0 +1,40 @@
+from kivymd.app import MDApp
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.uix.screenmanager import ScreenManager
+from kivymd.uix.screen import MDScreen
+from kivy.lang import Builder
+from login import LoginScreen
+from sign_up_screen import SignUpScreen
+from recovery_password_screen import RecoverPasswordScreen
+#from kivy.core.window import Window
+#Window.size = (350, 600)
+root = Builder.load_string("""
+<GatewayScreen>:
+    MDBoxLayout:
+    	orientation:"vertical"
+	    md_bg_color:[47/float(255), 79/float(255), 79/float(255), 1]
+	    MDBoxLayout:
+	        size_hint_y:None
+	        height:"100dp"
+	        md_bg_color:0, 0, 0, 1
+	        MDLabel:
+	        	text:"Xenopay"
+	        	text_size:self.size
+	        	font_size:"25dp"
+	        	halign:"center"
+	        	valign:"middle"
+	        	color:1, 1, 1, 1
+	    MDBoxLayout:
+	        ScreenManager:
+	            LoginScreen:
+	            SignUpScreen:
+	            RecoverPasswordScreen:
+""")
+class GatewayScreen(MDScreen):
+    pass
+class TestApp(MDApp):
+	def build(self):
+		root = GatewayScreen()
+		return root
+if __name__ == "__main__":
+	TestApp().run()
