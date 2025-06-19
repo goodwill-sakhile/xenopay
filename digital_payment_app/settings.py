75a9c764317525049773d4dad8b650eab88cc788
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-06-20 00:19:34 +0200

    settings script

--- digital_payment_app/settings.py
+++ digital_payment_app/settings.py
@@ -1,1 +1,300 @@
 
+SECRET_KEY = 'fake-key-0'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-1'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-2'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-3'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-4'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-5'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-6'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-7'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-8'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-9'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-10'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-11'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-12'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-13'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-14'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-15'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-16'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-17'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-18'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-19'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-20'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-21'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-22'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-23'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-24'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-25'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-26'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-27'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-28'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-29'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-30'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-31'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-32'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-33'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-34'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-35'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-36'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-37'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-38'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-39'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-40'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-41'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-42'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-43'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-44'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-45'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-46'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-47'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-48'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-49'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-50'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-51'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-52'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-53'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-54'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-55'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-56'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-57'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-58'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-59'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-60'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-61'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-62'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-63'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-64'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-65'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-66'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-67'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-68'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-69'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-70'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-71'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-72'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-73'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-74'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-75'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-76'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-77'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-78'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-79'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-80'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-81'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-82'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-83'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-84'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-85'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-86'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-87'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-88'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-89'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-90'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-91'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-92'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-93'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-94'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-95'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-96'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-97'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-98'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
+
+SECRET_KEY = 'fake-key-99'
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'payments', 'users', 'transactions']
