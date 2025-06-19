2fc65c22803ad0217fe4112fbac48fd6fe4082f8
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-06-20 00:36:23 +0200

    admin script

---
+++ transactions/admin.py
@@ -0,0 +1,500 @@
+
+from django.contrib import admin
+from .models import SampleModel0
+
+admin.site.register(SampleModel0)
+
+from django.contrib import admin
+from .models import SampleModel1
+
+admin.site.register(SampleModel1)
+
+from django.contrib import admin
+from .models import SampleModel2
+
+admin.site.register(SampleModel2)
+
+from django.contrib import admin
+from .models import SampleModel3
+
+admin.site.register(SampleModel3)
+
+from django.contrib import admin
+from .models import SampleModel4
+
+admin.site.register(SampleModel4)
+
+from django.contrib import admin
+from .models import SampleModel5
+
+admin.site.register(SampleModel5)
+
+from django.contrib import admin
+from .models import SampleModel6
+
+admin.site.register(SampleModel6)
+
+from django.contrib import admin
+from .models import SampleModel7
+
+admin.site.register(SampleModel7)
+
+from django.contrib import admin
+from .models import SampleModel8
+
+admin.site.register(SampleModel8)
+
+from django.contrib import admin
+from .models import SampleModel9
+
+admin.site.register(SampleModel9)
+
+from django.contrib import admin
+from .models import SampleModel10
+
+admin.site.register(SampleModel10)
+
+from django.contrib import admin
+from .models import SampleModel11
+
+admin.site.register(SampleModel11)
+
+from django.contrib import admin
+from .models import SampleModel12
+
+admin.site.register(SampleModel12)
+
+from django.contrib import admin
+from .models import SampleModel13
+
+admin.site.register(SampleModel13)
+
+from django.contrib import admin
+from .models import SampleModel14
+
+admin.site.register(SampleModel14)
+
+from django.contrib import admin
+from .models import SampleModel15
+
+admin.site.register(SampleModel15)
+
+from django.contrib import admin
+from .models import SampleModel16
+
+admin.site.register(SampleModel16)
+
+from django.contrib import admin
+from .models import SampleModel17
+
+admin.site.register(SampleModel17)
+
+from django.contrib import admin
+from .models import SampleModel18
+
+admin.site.register(SampleModel18)
+
+from django.contrib import admin
+from .models import SampleModel19
+
+admin.site.register(SampleModel19)
+
+from django.contrib import admin
+from .models import SampleModel20
+
+admin.site.register(SampleModel20)
+
+from django.contrib import admin
+from .models import SampleModel21
+
+admin.site.register(SampleModel21)
+
+from django.contrib import admin
+from .models import SampleModel22
+
+admin.site.register(SampleModel22)
+
+from django.contrib import admin
+from .models import SampleModel23
+
+admin.site.register(SampleModel23)
+
+from django.contrib import admin
+from .models import SampleModel24
+
+admin.site.register(SampleModel24)
+
+from django.contrib import admin
+from .models import SampleModel25
+
+admin.site.register(SampleModel25)
+
+from django.contrib import admin
+from .models import SampleModel26
+
+admin.site.register(SampleModel26)
+
+from django.contrib import admin
+from .models import SampleModel27
+
+admin.site.register(SampleModel27)
+
+from django.contrib import admin
+from .models import SampleModel28
+
+admin.site.register(SampleModel28)
+
+from django.contrib import admin
+from .models import SampleModel29
+
+admin.site.register(SampleModel29)
+
+from django.contrib import admin
+from .models import SampleModel30
+
+admin.site.register(SampleModel30)
+
+from django.contrib import admin
+from .models import SampleModel31
+
+admin.site.register(SampleModel31)
+
+from django.contrib import admin
+from .models import SampleModel32
+
+admin.site.register(SampleModel32)
+
+from django.contrib import admin
+from .models import SampleModel33
+
+admin.site.register(SampleModel33)
+
+from django.contrib import admin
+from .models import SampleModel34
+
+admin.site.register(SampleModel34)
+
+from django.contrib import admin
+from .models import SampleModel35
+
+admin.site.register(SampleModel35)
+
+from django.contrib import admin
+from .models import SampleModel36
+
+admin.site.register(SampleModel36)
+
+from django.contrib import admin
+from .models import SampleModel37
+
+admin.site.register(SampleModel37)
+
+from django.contrib import admin
+from .models import SampleModel38
+
+admin.site.register(SampleModel38)
+
+from django.contrib import admin
+from .models import SampleModel39
+
+admin.site.register(SampleModel39)
+
+from django.contrib import admin
+from .models import SampleModel40
+
+admin.site.register(SampleModel40)
+
+from django.contrib import admin
+from .models import SampleModel41
+
+admin.site.register(SampleModel41)
+
+from django.contrib import admin
+from .models import SampleModel42
+
+admin.site.register(SampleModel42)
+
+from django.contrib import admin
+from .models import SampleModel43
+
+admin.site.register(SampleModel43)
+
+from django.contrib import admin
+from .models import SampleModel44
+
+admin.site.register(SampleModel44)
+
+from django.contrib import admin
+from .models import SampleModel45
+
+admin.site.register(SampleModel45)
+
+from django.contrib import admin
+from .models import SampleModel46
+
+admin.site.register(SampleModel46)
+
+from django.contrib import admin
+from .models import SampleModel47
+
+admin.site.register(SampleModel47)
+
+from django.contrib import admin
+from .models import SampleModel48
+
+admin.site.register(SampleModel48)
+
+from django.contrib import admin
+from .models import SampleModel49
+
+admin.site.register(SampleModel49)
+
+from django.contrib import admin
+from .models import SampleModel50
+
+admin.site.register(SampleModel50)
+
+from django.contrib import admin
+from .models import SampleModel51
+
+admin.site.register(SampleModel51)
+
+from django.contrib import admin
+from .models import SampleModel52
+
+admin.site.register(SampleModel52)
+
+from django.contrib import admin
+from .models import SampleModel53
+
+admin.site.register(SampleModel53)
+
+from django.contrib import admin
+from .models import SampleModel54
+
+admin.site.register(SampleModel54)
+
+from django.contrib import admin
+from .models import SampleModel55
+
+admin.site.register(SampleModel55)
+
+from django.contrib import admin
+from .models import SampleModel56
+
+admin.site.register(SampleModel56)
+
+from django.contrib import admin
+from .models import SampleModel57
+
+admin.site.register(SampleModel57)
+
+from django.contrib import admin
+from .models import SampleModel58
+
+admin.site.register(SampleModel58)
+
+from django.contrib import admin
+from .models import SampleModel59
+
+admin.site.register(SampleModel59)
+
+from django.contrib import admin
+from .models import SampleModel60
+
+admin.site.register(SampleModel60)
+
+from django.contrib import admin
+from .models import SampleModel61
+
+admin.site.register(SampleModel61)
+
+from django.contrib import admin
+from .models import SampleModel62
+
+admin.site.register(SampleModel62)
+
+from django.contrib import admin
+from .models import SampleModel63
+
+admin.site.register(SampleModel63)
+
+from django.contrib import admin
+from .models import SampleModel64
+
+admin.site.register(SampleModel64)
+
+from django.contrib import admin
+from .models import SampleModel65
+
+admin.site.register(SampleModel65)
+
+from django.contrib import admin
+from .models import SampleModel66
+
+admin.site.register(SampleModel66)
+
+from django.contrib import admin
+from .models import SampleModel67
+
+admin.site.register(SampleModel67)
+
+from django.contrib import admin
+from .models import SampleModel68
+
+admin.site.register(SampleModel68)
+
+from django.contrib import admin
+from .models import SampleModel69
+
+admin.site.register(SampleModel69)
+
+from django.contrib import admin
+from .models import SampleModel70
+
+admin.site.register(SampleModel70)
+
+from django.contrib import admin
+from .models import SampleModel71
+
+admin.site.register(SampleModel71)
+
+from django.contrib import admin
+from .models import SampleModel72
+
+admin.site.register(SampleModel72)
+
+from django.contrib import admin
+from .models import SampleModel73
+
+admin.site.register(SampleModel73)
+
+from django.contrib import admin
+from .models import SampleModel74
+
+admin.site.register(SampleModel74)
+
+from django.contrib import admin
+from .models import SampleModel75
+
+admin.site.register(SampleModel75)
+
+from django.contrib import admin
+from .models import SampleModel76
+
+admin.site.register(SampleModel76)
+
+from django.contrib import admin
+from .models import SampleModel77
+
+admin.site.register(SampleModel77)
+
+from django.contrib import admin
+from .models import SampleModel78
+
+admin.site.register(SampleModel78)
+
+from django.contrib import admin
+from .models import SampleModel79
+
+admin.site.register(SampleModel79)
+
+from django.contrib import admin
+from .models import SampleModel80
+
+admin.site.register(SampleModel80)
+
+from django.contrib import admin
+from .models import SampleModel81
+
+admin.site.register(SampleModel81)
+
+from django.contrib import admin
+from .models import SampleModel82
+
+admin.site.register(SampleModel82)
+
+from django.contrib import admin
+from .models import SampleModel83
+
+admin.site.register(SampleModel83)
+
+from django.contrib import admin
+from .models import SampleModel84
+
+admin.site.register(SampleModel84)
+
+from django.contrib import admin
+from .models import SampleModel85
+
+admin.site.register(SampleModel85)
+
+from django.contrib import admin
+from .models import SampleModel86
+
+admin.site.register(SampleModel86)
+
+from django.contrib import admin
+from .models import SampleModel87
+
+admin.site.register(SampleModel87)
+
+from django.contrib import admin
+from .models import SampleModel88
+
+admin.site.register(SampleModel88)
+
+from django.contrib import admin
+from .models import SampleModel89
+
+admin.site.register(SampleModel89)
+
+from django.contrib import admin
+from .models import SampleModel90
+
+admin.site.register(SampleModel90)
+
+from django.contrib import admin
+from .models import SampleModel91
+
+admin.site.register(SampleModel91)
+
+from django.contrib import admin
+from .models import SampleModel92
+
+admin.site.register(SampleModel92)
+
+from django.contrib import admin
+from .models import SampleModel93
+
+admin.site.register(SampleModel93)
+
+from django.contrib import admin
+from .models import SampleModel94
+
+admin.site.register(SampleModel94)
+
+from django.contrib import admin
+from .models import SampleModel95
+
+admin.site.register(SampleModel95)
+
+from django.contrib import admin
+from .models import SampleModel96
+
+admin.site.register(SampleModel96)
+
+from django.contrib import admin
+from .models import SampleModel97
+
+admin.site.register(SampleModel97)
+
+from django.contrib import admin
+from .models import SampleModel98
+
+admin.site.register(SampleModel98)
+
+from django.contrib import admin
+from .models import SampleModel99
+
+admin.site.register(SampleModel99)
